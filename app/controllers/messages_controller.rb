96b90d45c2b200aefdc83215d4577aa1d676eeb3
hedgehog-genki <84561429+hedgehog-genki@users.noreply.github.com>
2021-05-25 15:35:14 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -11,9 +12,10 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_message_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :index
     end
-    
+
   end
 
   private
